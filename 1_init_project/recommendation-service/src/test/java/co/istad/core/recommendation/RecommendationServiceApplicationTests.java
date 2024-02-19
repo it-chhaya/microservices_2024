e67068e6b1234bad11c73e2ae9d001449fd02572
it.chhaya <it.chhaya@gmail.com>
2024-02-19 18:00:37 +0700

    first commit

---
+++ 1_init_project/recommendation-service/src/test/java/co/istad/core/recommendation/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.core.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 1_init_project/review-service/src/test/java/co/istad/core/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.core.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
